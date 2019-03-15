a4f90f488d23ffeb691462ba7d2ab90e39801608
Colby <colbytaylor1212@gmail.com>
2019-03-15 14:04:14 -0400

    made some style changes that shouldve been done on dev

--- js/main.js
+++ js/main.js
@@ -3,3 +3,14 @@ function show(shown, hidden) {
     document.getElementById(hidden).style.display='none';
     return false;
 }
+
+const url = 'https://use.fontawesome.com/releases/v5.7.2/css/all.css'
+function list() {
+    var x = document.createElement("LI");
+
+
+    document.getElementById("aList").appendChild(x);
+}
+
+
+var igloo = window.FontAwesome.icon({ prefix: 'fas', iconName: 'igloo' })
